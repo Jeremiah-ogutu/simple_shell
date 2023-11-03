fd7520a100dca22bdf755a5f0f45d79c138a316f
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-11-03 15:59:03 +0300

    update of shell.h

--- shell.h
+++ shell.h
@@ -5,4 +5,13 @@
 #include "string.h"
 #include "unistd.h"
 
+
+void shell_printf(const char *outPut);
+void shell_prompt(void);
+void execute_prompt(const char *outPut);
+void user_inPut(char *inPut, size_t size);
+
+
+
+
 #endif
