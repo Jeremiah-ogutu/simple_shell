4544c8e0d0f52e9cada7c92a98b10b9a6c723556
LisetteMurugi <lisettendwiga4@gmail.com>
2023-11-14 12:42:45 +0300

    execute corrections

--- execute.c
+++ execute.c
@@ -2,33 +2,21 @@
 
 void executing_text(const char *text)
 {
-	pid_t child_pid = fork();
+        pid_t child_pid = fork();
 
-	if (child_pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (child_pid == 0)
-	{
-		char *text_args[150];
-		int args_count = 0;
-
-		char *token = strtok((char *)text, " ");
-		while (token != NULL)
-		{
-			text_args[args_count++] = token;
-			token = strtok(NULL, " ");
-		}
-		text_args[args_count] = NULL;
-
-		execvp(text_args[0], text_args);
-		perror("execvp");
-
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		wait(NULL);
-	}
+        if (child_pid == -1)
+        {
+                perror("fork");
+                exit(EXIT_FAILURE);
+        }
+        else if (child_pid == 0)
+        {
+                execve(text, text, (char *)NULL);
+                perror("execve");
+                exit(EXIT_FAILURE);
+        }
+        else
+        {
+                wait(NULL);
+        }
 }
