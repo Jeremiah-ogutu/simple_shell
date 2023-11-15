0b82c3cf39a40891304123b59e17186e06ad4409
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-11-15 15:02:45 +0300

    execute.c

--- execute.c
+++ execute.c
@@ -6,7 +6,7 @@
 * @arguments:the txt in the function
 */
 
-void executing_text(const char *command, char *const arguments[])
+void executing_text(const char *text)
 {
 pid_t child_pid = fork();
 
@@ -17,8 +17,20 @@ exit(EXIT_FAILURE);
 }
 else if (child_pid == 0)
 {
-execve(command, arguments, NULL);
-perror("execve");
+char *text_args[150];
+int args_count = 0;
+
+char *token = strtok((char *)text, " ");
+while (token != NULL)
+{
+text_args[args_count++] = token;
+token = strtok(NULL, " ");
+}
+text_args[args_count] = NULL;
+
+execvp(text_args[0], text_args);
+perror("execvp");
+
 exit(EXIT_FAILURE);
 }
 else
